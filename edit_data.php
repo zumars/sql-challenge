420b3a18f7999e808de1a3e68c8fb2ce9c892646
zumars <marshalruiz@outlook.com>
2016-11-18 20:02:59 -0700

    Create edit_data.php

---
+++ edit_data.php
@@ -0,0 +1,84 @@
+<?php
+include_once 'dbconfig.php';
+if(isset($_GET['edit_id']))
+{
+ $sql_query="SELECT * FROM users WHERE user_id=".$_GET['edit_id'];
+ $result_set=mysql_query($sql_query);
+ $fetched_row=mysql_fetch_array($result_set);
+}
+if(isset($_POST['btn-update']))
+{
+
+ $Title = $_POST['Title'];
+ $Text = $_POST['Text'];
+
+
+ 
+ $sql_query = "UPDATE users SET first_name='$first_name',last_name='$last_name',user_city='$city_name' WHERE user_id=".$_GET['edit_id'];
+
+ 
+
+ if(mysql_query($sql_query))
+ {
+  ?>
+  <script type="text/javascript">
+  alert('Data Are Updated Successfully');
+  window.location.href='index.php';
+  </script>
+  <?php
+ }
+ else
+ {
+  ?>
+  <script type="text/javascript">
+  alert('error occured while updating data');
+  </script>
+  <?php
+ }
+
+}
+if(isset($_POST['btn-cancel']))
+{
+ header("Location: index.php");
+}
+?>
+<!DOCTYPE>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="header">
+ <div id="content">
+    <label>Blog</label>
+    </div>
+</div>
+
+<div id="body">
+ <div id="content">
+    <form method="post">
+    <table align="center">
+    <tr>
+    <td><input type="text" name="Title" placeholder="Title" value="<?php echo $fetched_row['Title']; ?>" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="Text" placeholder="Text" value="<?php echo $fetched_row['Text']; ?>" required /></td>
+    </tr>
+    <tr>
+    <td>
+    <button type="submit" name="btn-update"><strong>UPDATE</strong></button>
+    <button type="submit" name="btn-cancel"><strong>Cancel</strong></button>
+    </td>
+    </tr>
+    </table>
+    </form>
+    </div>
+</div>
+
+</center>
+</body>
+</html>
