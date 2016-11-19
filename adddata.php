34081c732040e765bb3bc9582a90262321b36364
zumars <marshalruiz@outlook.com>
2016-11-18 19:55:29 -0700

    Create adddata.php

---
+++ adddata.php
@@ -0,0 +1,38 @@
+<!DOCTYPE>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Blog</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="header">
+ <div id="content">
+    <label>Blog</label>
+    </div>
+</div>
+<div id="body">
+ <div id="content">
+    <form method="post">
+    <table align="center">
+    <tr>
+    <td align="center"><a href="index.php">back to main page</a></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="Title" placeholder="First Name" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="text" placeholder="Last Name" required /></td>
+    </tr>
+    <tr>
+    <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
+    </tr>
+    </table>
+    </form>
+    </div>
+</div>
+
+</center>
+</body>
