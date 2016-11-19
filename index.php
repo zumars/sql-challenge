e8a169b2120ee296f920daf5482eef4cc8d487b4
zumars <marshalruiz@outlook.com>
2016-11-18 19:58:03 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+include_once 'dbconfig.php';
+?>
+<!DOCTYPE>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>BLog</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<center>
+
+<div id="body">
+ <div id="content">
+    <table align="center">
+    <tr>
+    <th colspan="5"><a href="adddata.php">add data here.</a></th>
+    </tr>
+    <th>Title</th>
+    <th>Text</th>
+    <th colspan="2">Operations</th>
+    </tr>
+    <?php
+ $sql_query="SELECT * FROM users";
+ $result_set=mysql_query($sql_query);
+ while($row=mysql_fetch_row($result_set))
+ {
+  ?>
+        <tr>
+        <td><?php echo $row[1]; ?></td>
+        <td><?php echo $row[2]; ?></td>
+  <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
+        <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
+        </tr>
+        <?php
+ }
+ ?>
+    </table>
+    </div>
+</div>
+
+</center>
+</body>
+</html>
